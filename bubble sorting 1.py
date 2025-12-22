83e14699f54e617bd8839ca48b6c6e92c9444d67
BandariDeepika <deepikabandari3371@gmail.com>
2025-12-22 22:53:19 +0530

    Create bubble sorting 1.py

---
+++ bubble sorting 1.py
@@ -0,0 +1,12 @@
+#ascending order to desecending 
+def sorting(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr)-1-i):
+            if arr[j]<arr[j+1]:
+                new=arr[j]
+                arr[j]=arr[j+1]
+                arr[j+1]=new
+arr=[21,42,44,49,23,0,-1,17]
+sorting(arr)
+for i in range(len(arr)):
+    print(arr[i],end=" ")
