c04c4096a7f6ebbc7bfad898a3ffc5d5a8684022
BandariDeepika <deepikabandari3371@gmail.com>
2025-12-23 15:40:12 +0530

    Create Linear Search.py

---
+++ Linear Search.py
@@ -0,0 +1,10 @@
+my_list[23,45,67,89,56]
+falg=False
+search_element=67
+for element==search_element:
+   if element==search_element:
+      print(search_element,"found successfully")
+      flag=True
+      break
+if not flag:
+  print(search_element,is not found")
