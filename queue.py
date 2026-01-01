b8116166c6de5be4578b6628ee6c7ae7046a9af1
DeepikaBandari <deepikabandari3371@gmail.com>
2026-01-01 18:39:45 +0530

    Create queue.py

---
+++ queue.py
@@ -0,0 +1,62 @@
+#queues
+'''queue=[]
+queue.append(10)
+queue.append(20)
+queue.append(30)
+queue.append(40)
+while queue:
+    print(queue[0])
+    queue.pop(0)'''
+
+'''def enqueue(ele):
+    queue.append(ele)
+def dequeue():
+    print(queue[0])
+    queue.pop(0)
+queue=[]
+enqueue(10)
+enqueue(20)
+enqueue(30)
+enqueue(40)
+dequeue()'''
+
+#queue to stack implementation
+'''def enqueue(ele):
+    st1.append(ele)
+def dequeue():
+    if st2==[]:
+        while st1:
+            st2.append(st1[-1])
+            st1.pop()
+    print(st2[-1])
+    st2.pop()
+st1=[]
+st2=[]
+enqueue(10)
+enqueue(20)
+enqueue(30)
+enqueue(40)
+dequeue()
+dequeue()
+dequeue()
+dequeue()'''
+
+# print in queue in reverse order
+'''def enqueue(ele):
+    str1.append(ele)
+def dequeue():
+    while str1:
+        print(str1[-1])
+        str1.pop()
+str1=[]
+enqueue(10)
+enqueue(20)
+enqueue(30)
+enqueue(40)
+dequeue()
+dequeue()
+dequeue()
+dequeue()'''
+
+'''a=8
+print(a>>2)'''
